8553ad0185805ad2444f4c6ec78adf2eb146fd1b
Wahid Magdy <wahidmagdy@gmail.com>
2021-03-04 10:48:17 +0200

    Add 404 redirection to home
    Added a handler for 404 pages, automatically redirects back to home

---
+++ src/pages/404.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect } from "react";
+import { navigate } from "gatsby";
+
+export default function NotFoundPage() {
+  useEffect(() => {
+    navigate("/"); // redirecting to home page
+  }, []);
+
+  return (
+    <div>
+      <h1>(404) NotFound Page</h1>
+    </div>
+  );
+}
